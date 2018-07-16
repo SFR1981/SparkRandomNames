04ea1e41723230225e3b58f138fc625d5a604e86
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-16 16:46:16 +0100

    added new route for infinite random names from finite list

--- src/main/java/NameController.java
+++ src/main/java/NameController.java
@@ -1,6 +1,7 @@
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static spark.Spark.get;
@@ -21,7 +22,30 @@ public class NameController {
             HashMap<String, Object> model = new HashMap<>();
             String name1 = name.getRandomName();
             model.put("name", name1);
-            return new ModelAndView(model, "public/one.vtl");
+
+            return new ModelAndView(model, "one.vtl");
+        }, velocityTemplateEngine);
+
+        get("/two", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            String names = name.getRandomPair();
+            model.put("names", names);
+
+            return new ModelAndView(model, "two.vtl");
+        }, velocityTemplateEngine);
+
+            get("/:n", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            int number = Integer.parseInt(req.params(":n"));
+            ArrayList<String> nameList = new ArrayList<>();
+            for (int i = 1; i <= number; i++) {
+                nameList.add(name.getRandomName());
+            }
+            String names = String.join(", " , nameList);
+            model.put("result", names);
+            return new ModelAndView(model, "result.vtl");
         }, velocityTemplateEngine);
 
 
