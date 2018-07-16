320c67f0f7c88745aa2569348ce7dd66ba1b7e9a
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-16 16:20:55 +0100

    basic random names appearing in one.vtl

--- src/main/java/NameController.java
+++ src/main/java/NameController.java
@@ -1,2 +1,30 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class NameController {
+
+
+    public static void main(String[] args) {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+        staticFileLocation("/public");
+
+        Name name = new Name();
+
+
+        get("/one", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            String name1 = name.getRandomName();
+            model.put("name", name1);
+            return new ModelAndView(model, "public/one.vtl");
+        }, velocityTemplateEngine);
+
+
+    }
 }
+
